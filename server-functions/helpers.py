0b9698999c8181d0be799d2a292164982a73a3c2
unknown <gamer7s.2005@gmail.com>
2023-08-30 14:45:12 +0300

    adding notification and reviews system

--- server-functions/helpers.py
+++ server-functions/helpers.py
@@ -1,15 +1,17 @@
 import json
 import hashlib
 
+import consts
 
-def get_all_users(path='users.json'):
+
+def get_all_users(path=consts.USERS_DB_PATH):
     with open(path, 'r') as users:
         all_users = json.loads(users.read())
 
     return all_users
 
 
-def get_user_id_counter(path='user_controller.json'):
+def get_user_id_counter(path=consts.USER_CONTROLLER_PATH):
     with open(path, 'r') as user_controller:
         id_counter = json.loads(user_controller.read())['id_counter']
 
@@ -34,8 +36,8 @@ def check_username_and_email(username, email):
     return True
 
 
-def add_new_user_to_db(user_object, user_path='users.json',
-                       user_controller_path='user_controller.json'):
+def add_new_user_to_db(user_object, user_path=consts.USERS_DB_PATH,
+                       user_controller_path=consts.USER_CONTROLLER_PATH):
     users = get_all_users()
 
     if check_username_and_email(user_object['username'], user_object['email']):
@@ -44,7 +46,8 @@ def add_new_user_to_db(user_object, user_path='users.json',
 
         current_users_id_counter = get_user_id_counter()
 
-        users.append({**user_object, "id": current_users_id_counter + 1})
+        users.append(
+            {**user_object, "id": current_users_id_counter + 1, "notification": [], "reviews": [], "reviews_to_do": []})
 
         with open(user_controller_path, 'w') as user_controller:
             user_controller.write(json.dumps({"id_counter": current_users_id_counter + 1}))
@@ -66,19 +69,86 @@ def get_user_by_id(user_id):
     users = get_all_users()
 
     if not len(users):
-        return {}
+        return {}, -1
 
     low = 0
-    high = len(users)
+    high = len(users)-1
 
-    while low < high:
+    while low <= high:
         mid = (low + high) // 2
 
         if users[mid]['id'] == user_id:
-            return users[mid]
+            return users[mid], mid
         elif users[mid]['id'] > user_id:
-            high = mid-1
+            high = mid - 1
         elif users[mid]['id'] < user_id:
-            low = mid+1
+            low = mid + 1
 
-    return {}
+    return {}, -1
+
+
+def get_user_notification(user_id):
+    return get_user_by_id(user_id)[0]['notification']
+
+
+def get_user_reviews(user_id):
+    return get_user_by_id(user_id)[0]['reviews']
+
+
+def add_notification_to_user(user_id, notification_message, user_path=consts.USERS_DB_PATH):
+    user, user_index = get_user_by_id(user_id)
+
+    if not user:
+        return f'There is no user with id {user_id}'
+
+    users = get_all_users()
+
+    user['notification'].append(notification_message)
+    users[user_index] = user
+
+    with open(user_path, 'w') as users_file:
+        users_file.write(json.dumps(users))
+
+    return get_user_notification(user_id)
+
+
+def add_review_to_user(receiver_id, review_dict, user_path=consts.USERS_DB_PATH):
+    sender, sender_index = get_user_by_id(review_dict["from"])
+    receiver, receiver_index = get_user_by_id(receiver_id)
+
+    if not sender or not receiver:
+        if not sender:
+            return f'There is no user with {review_dict["from"]}'
+        else:
+            return f'There is no user with id {receiver_id}'
+
+    users = get_all_users()
+
+    receiver['reviews'].append({"from": review_dict["from"], "message": review_dict["message"]})
+    sender['reviews_to_do'].pop(review_dict["review_index"])
+    users[receiver_index] = receiver
+    users[sender_index] = sender
+
+    with open(user_path, 'w') as users_file:
+        users_file.write(json.dumps(users))
+
+    return get_user_reviews(receiver_id)
+
+
+def add_to_do_review_to_user(user_id, receiver_id, user_path=consts.USERS_DB_PATH):
+    user, user_index = get_user_by_id(user_id)
+
+    if not user or not get_user_by_id(receiver_id)[0]:
+        if not user:
+            return f'There is no user with id {user_id}'
+        else:
+            return f'There is no user with id {receiver_id}'
+
+    users = get_all_users()
+
+    user['reviews_to_do'].append({"for": receiver_id, "message": ""})
+    users[user_index] = user
+    print(users)
+
+    with open(user_path, 'w') as users_file:
+        users_file.write(json.dumps(users))
